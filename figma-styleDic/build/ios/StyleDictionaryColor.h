6ce110e00480ad81c73258334ea45b22787c858c
mrazian85 <mrazian@avaya.com>
2020-05-05 10:59:13 -0400

    Commit before branching for style-dictionary

---
+++ figma-styleDic/build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+// Do not edit directly
+// Generated on Tue, 05 May 2020 14:58:10 GMT
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
