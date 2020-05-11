f078c64022f56bf13c9b47eef57954e496adb663
mrazian85 <mrazian@avaya.com>
2020-05-10 20:44:42 -0400

    Added class creation functionality

--- styleDicExt.js
+++ styleDicExt.js
@@ -9,11 +9,18 @@ const _ = require('lodash');
 console.log('Build started.....');
 console.log('\n=======================================');
 
+// this is where we create a Format to create classes
+
+const fileHeader =
+  '// Mo generated this file automatically on ' + new Date().toUTCString + '\n';
+
+const template = _.template(
+  fs.readFileSync(__dirname + '/templates/web-scss.template')
+);
+
 StyleDictionary.registerFormat({
   name: 'custom/format/scss',
-  formatter: _.template(
-    fs.readFileSync(__dirname + '/templates/web-scss.template')
-  ),
+  formatter: template,
 });
 
 StyleDictionary.registerFilter({
