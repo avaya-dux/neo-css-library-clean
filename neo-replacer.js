ef0e407f1347af6d9902cdf7fd6fb98716952ee9
mrazian85 <mrazian@avaya.com>
2020-08-04 07:56:47 -0400

    Tweaked Neo styles as per feedback

--- neo-replacer.js
+++ neo-replacer.js
@@ -1,73 +1,23 @@
-//Code to try and replace tokens in Neo
+//Code to edit NEO .css file as necessary after creation
+
+// import replace-in-file module
 
 const replace = require('replace-in-file');
 
-// we import all tokens as a js =bject
+// create variable for ':root' RegEx
 
-// const tokens = require('./build/js/tokens');
+var rootRegEx = new RegExp(':root', 'g');
 
-// #region Code to replace colors
-
-// we get the name of a particular token, in this case semGreenDark
-
-// var semGreenDark = Object.keys(tokens)[11];
-
-// // we save the token's name and value in a Regular Expression
-
-// var tokenRegExName = new RegExp(semGreenDark, 'g');
-
-// var tokenRegExValue = new RegExp(tokens[semGreenDark], 'g');
-
-// // change style To -- we write functions that change the style from what it is in current Neo
-// // to what we want it to be
-
-// // TODO -- replace scss directly with css file
-
-// const optionsToName = {
-//   files: './neo/scss/**/*.scss',
-//   from: /neo-presence-red/g,
-//   to: semGreenDark,
-// };
-
-// const optionsToValue = {
-//   files: './neo/scss/**/*.scss',
-//   from: /#d50000/g,
-//   to: tokens[semGreenDark],
-// };
-
-// // change style From -- we write functions that revert the style back to current Neo
-
-// const optionsFromName = {
-//   files: './neo/scss/**/*.scss',
-//   from: tokenRegExName,
-//   to: 'neo-presence-red',
-// };
-
-// const optionsFromValue = {
-//   files: './neo/scss/**/*.scss',
-//   from: tokenRegExValue,
-//   to: '#d50000',
-// };
-
-// we call the function as necessary
+// create Object to use as function parameter
 
 const deleteRoot = {
-  files: './neo/scss/**/*.scss',
-  from: /neo-presence-red/g,
-  to: semGreenDark,
+  files: '../Avaya_Design_Portal_proto/DT_gen_files/combined.css',
+  from: rootRegEx,
+  to: '',
 };
 
-// replace(optionsToName).then((results) => {
-//   console.log('Replacement results:', results);
+// run replace function to delete the ':root' string
 
-//   replace(optionsToValue).then((results) => {
-//     console.log('******************************\n');
-
-//     console.log('Replacement results:', results);
-//   });
-// });
-
-// #endregion
-
-// might have to use this to replace entire files as opposed to Strings in files
-// could replace colors files for example
+replace(deleteRoot).then((results) => {
+  console.log('Replacement results:', results);
+});
