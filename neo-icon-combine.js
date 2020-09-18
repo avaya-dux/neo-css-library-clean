e234af1e65504e5fab86e38da215763120aedf45
mrazian85 <mrazian@avaya.com>
2020-09-18 08:45:16 -0400

    Streamlining build process

--- neo-icon-combine.js
+++ neo-icon-combine.js
@@ -1,3 +1,14 @@
 const fs = require('fs').promises;
 
-const iconsFile = await fs.readFile('./build/css/updated-neo-icons.css');
+const iconsFile = async () =>
+  await fs
+    .readFile('./build/css/updated-neo-icons.css')
+    .then(async (result) => {
+      //   console.log(result.toString());
+      await fs.appendFile('./test.txt', result.toString());
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+iconsFile();
