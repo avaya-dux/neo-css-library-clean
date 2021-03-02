fbfe6631402505b36fe32844ba0f6e71e0edf35d
mrazian85 <mrazian@avaya.com>
2021-03-02 12:59:38 -0500

    Added new icons, tweaked padding and margins of inputs in inline forms to preserve alignment, tweaked positioning of radio buttons to preserve alignment, added new focus styles to radios/checkboxes/switches

--- API/components-functions/table_variants.js
+++ API/components-functions/table_variants.js
@@ -42,6 +42,36 @@ $table-date-picker-font-color: $token-color-base-600 !default;
 $table-sortable-ascending-icon: $token-icons-neo-icon-chevron-up !default;
 $table-sortable-descending-icon: $token-icons-neo-icon-chevron-down !default;
 
+ // th background color - table.background-color - DONE
+ // th padding - DONE
+ // th font styles - DONE
+ // th min height - DONE
+ // th border styles - DONE
+ // th hover color - DONE
+
+ // th filter multiselect font styles - DONE
+ // th filter multiselect spacing - DONE
+// th filter multiselect icon sizing & spacing - DONE
+
+// tr disabled color
+// tr border styles
+// tr hover/active color
+// tr active font weight
+
+// td padding
+// td link color
+
+// nested table padding
+// nested table border style
+// nested table row background color
+// nasted table inside row max height
+// nested table row cell padding
+
+// filters sheet header spacing
+// filters sheet form padding
+// filters sheet border style
+
+// actions spacing
 
 */
 
@@ -52,26 +82,181 @@ async function tableStyles(value) {
 
   for (component of value.Table.children) {
     // TABLE ACTION ROW
-    if (component.name === '')
-      if (component.name === '.row-text-base') {
-        // console.log(component);
-        // table background
-        var tableBackgroundColorTokenID = component.styles.fills;
-        await coreFigmaFunctions
-          .getFigmaTokenNameByID(
-            coreFigmaFunctions.figmaCredentials.figmaAPIKey,
-            coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
-            tableBackgroundColorTokenID
-          )
-          .then(
-            (value) =>
-              (tableJSONObject.table['background-color'] = {
+    if (component.name === '.row-text-base') {
+      // console.log(component);
+      // table background
+      var tableBackgroundColorTokenID = component.styles.fills;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          tableBackgroundColorTokenID
+        )
+        .then(
+          (value) =>
+            (tableJSONObject.table['background-color'] = {
+              value: `{color.${value.nodes[
+                tableBackgroundColorTokenID
+              ].document.name.toLowerCase()}.value}`,
+            })
+        );
+    }
+    if (component.name === '.header-base') {
+      // console.log(component.children[1]);
+      // console.log(component.children[0]);
+      // header cell padding
+      tableJSONObject.table['header-cell-padding-x'] = {
+        value: `${component.children[0].relativeTransform[0][2]}px`,
+      };
+      tableJSONObject.table['header-cell-padding-y '] = {
+        value: `${component.children[0].relativeTransform[1][2]}px`,
+      };
+      // header cell font styles
+      var headerFontTokenID = component.children[0].children[0].styles.text;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          headerFontTokenID
+        )
+        .then((value) => {
+          var headerFontTokenName = value.nodes[
+            headerFontTokenID
+          ].document.name.toLowerCase();
+          // header-font-size
+          tableJSONObject.table['header-cell-font-size'] = {
+            value: `{Web-typography.${headerFontTokenName}.fontSize.value}`,
+          };
+          // header-line-height
+          tableJSONObject.table['header-cell-line-height'] = {
+            value: `{Web-typography.${headerFontTokenName}.lineHeight.value}`,
+          };
+          // header-letter-spacing
+          tableJSONObject.table['header-cell-letter-spacing'] = {
+            value: `{Web-typography.${headerFontTokenName}.letterSpacing.value}`,
+          };
+          // header-font-weight
+          tableJSONObject.table['header-cell-font-weight'] = {
+            value: `{Web-typography.fontweight-regular.value}`,
+          };
+        });
+      // header cell min height
+      tableJSONObject.table['header-cell-min-height'] = {
+        value: `${component.size.y}px`,
+      };
+      // header border styles
+      // border width
+      tableJSONObject.table['header-cell-border-width'] = {
+        value: `${component.children[1].strokeWeight}px`,
+      };
+      // border style
+      tableJSONObject.table['header-cell-border-style'] = {
+        value: component.children[1].strokes[0].type.toLowerCase(),
+      };
+      // border color
+      var headerCellBorderColorID = component.children[1].styles.stroke;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          headerCellBorderColorID
+        )
+        .then((value) => {
+          tableJSONObject.table['header-cell-border-color'] = {
+            value: `{color.${value.nodes[
+              headerCellBorderColorID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+    }
+    if (component.name === 'column-header') {
+      // console.log(component);
+      for (variant of component.children) {
+        if (
+          variant.name ===
+          'Type=text, Can Sort/Filter=TRUE, Filter Applied=FALSE, Sorting Applied=none, State=hover, Location=main'
+        ) {
+          // console.log(variant.children[0]);
+          var headerCellHoverColorID = variant.children[0].styles.fills;
+          await coreFigmaFunctions
+            .getFigmaTokenNameByID(
+              coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+              coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+              headerCellHoverColorID
+            )
+            .then((value) => {
+              tableJSONObject.table['header-cell-hover-color'] = {
                 value: `{color.${value.nodes[
-                  tableBackgroundColorTokenID
+                  headerCellHoverColorID
                 ].document.name.toLowerCase()}.value}`,
-              })
-          );
+              };
+            });
+        }
       }
+    }
+    if (component.name === 'column-filter') {
+      // console.log(component);
+      for (variant of component.children) {
+        if (variant.name === 'Type=alphabetical, Active=none') {
+          // filter text styles
+          var textElement =
+            variant.children[0].children[0].children[1].children[1];
+          // console.log(textElement);
+          var filterFontTokenID = textElement.styles.text;
+          await coreFigmaFunctions
+            .getFigmaTokenNameByID(
+              coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+              coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+              filterFontTokenID
+            )
+            .then((value) => {
+              var filterFontTokenName = value.nodes[
+                filterFontTokenID
+              ].document.name.toLowerCase();
+              // header-font-size
+              tableJSONObject.table['filter-font-size'] = {
+                value: `{Web-typography.${filterFontTokenName}.fontSize.value}`,
+              };
+              // header-line-height
+              tableJSONObject.table['filter-line-height'] = {
+                value: `{Web-typography.${filterFontTokenName}.lineHeight.value}`,
+              };
+              // header-letter-spacing
+              tableJSONObject.table['filter-letter-spacing'] = {
+                value: `{Web-typography.${filterFontTokenName}.letterSpacing.value}`,
+              };
+              // header-font-weight
+              tableJSONObject.table['filter-font-weight'] = {
+                value: `{Web-typography.fontweight-regular.value}`,
+              };
+            });
+          // filter text spacing
+          tableJSONObject.table['filter-padding-vertical'] = {
+            value: `${textElement.relativeTransform[1][2]}px`,
+          };
+          // filter text margin
+          const filterTextMarginBottom =
+            textElement.relativeTransform[1][2] * 2;
+          tableJSONObject.table['filter-text-margin-bottom'] = {
+            value: `${filterTextMarginBottom}px`,
+          };
+        }
+        if (variant.name === 'Type=alphabetical, Active=first') {
+          // filter icon spacing
+          const filterIconSpacing =
+            variant.children[0].children[0].children[0].children[0]
+              .relativeTransform[0][2] +
+            variant.children[0].children[0].children[0].relativeTransform[0][2];
+          tableJSONObject.table['filter-icon-spacing'] = {
+            value: `${filterIconSpacing}px`,
+          };
+          // filter icon size
+          tableJSONObject.table['filter-icon-size'] = {
+            value: `${variant.children[0].children[0].children[0].children[0].size.x}px`,
+          };
+        }
+      }
+    }
   }
 
   // const tableVariants = value.Table.children.filter(
--- readme.md
+++ readme.md
@@ -1,1 +1,65 @@
 This is the program that generates the Neo UI Component library
+
+This program is comprised of a collection of related modules that work together in various capacities to pull the UX Team's designs from our design software, Figma.
+
+The salient elements of the source code file structure can be described as follows:
+
+API --> These are the files associated with pulling the information related to our designs from Figma, process them, and converting them to the desired format across platforms. These are the most important files in the project.
+
+components-functions --> This folder contains individual files related to each Component in Figma
+
+figma-functions --> This folder contains information related to Figma's API itself, including authentication and file information. It also includes files generated each time we pull our Design Tokens, which are then processed as necessary.
+
+icons-functions --> This folder contains files related to pulling our icons from Figma, generating the necessary icon font files and converting these into unicode values for use as CSS class name values.
+
+tokens-functions --> This folder contains the individual files associated with pulling each of our Design Tokens.
+
+firebase-functions --> This is a parallel development environment which represents the implementation of our Firebase server. It includes the functions for distributing our icon library as png/svg assets, as well as the code used to sign users up to our automated e-mail notification service.
+
+fonts --> Includes all the relevant font and CSS files for our multi-lingual font support
+
+neo --> This folder includes all the .scss files associated with the web version of our Neo UI Kit.
+
+original-neo-styles --> The original, unaltered .css and .scss files as received by UX team as Neo v.2.x
+
+scss-update --> The current collection of .scss files being used to generate each new iteration of our Neo UI Kit
+
+neo_zip --> These are the files that are compressed and uploaded to our site for download each time a new version of Neo is released. Currently updated and compressed manually.
+
+properties --> This folder contains the raw .json data generated from pulling our design information from Figma before it is run through style-dictionary and translated for each platform.
+
+templates --> This folder contains misc. templates used when generating our design files, including the .css template for our icon library
+
+//////// TO-DO -- address individual files in the project as necessary ////////
+
+Explanation of npm scripts:
+
+The following scripts, located in package.json, are responsible for generating our design system files. The critical ones are as follows:
+
+icongen --> This script pulls and generates .css files from our icon library -- NOTE: there are some necessary files that are run manually and which need to be included in this script for it to be complete.
+neo-update-site-fast --> This script generates a .css file from all the .scss files in the neo/scss-update folder and inserts them directly in our website file directory for rapid testing/updates
+
+/////// TO-DO -- continue enumerating and explaining scripts
+
+Draft explanation of order of operations when updating Neo UI library:
+
+When updating to a new version of the Neo UI library, the following steps constitute the usual order of operations:
+
+1. If a new Component is being created, a unique .js file is created in the /API/components-functions directory for the Component in question. This file will contain the code which pulls information from the relavant file in Figma
+2. Once the necessary properties are pulled as .json files, add the necessary configuration info to /config.json and run 'node -r esm styleDicExt.js' to generate the necessary .scss files
+3. Create the corresponding files and values in the /neo/scss-update folder -- these can be tested if the necessary HTML has been added to the '/updated-components' page on the website after running the 'neo-update-site-fast' script
+4. The file created by the above script can be added to an appropriate distribution folder ex. neo/dist/3.18.0
+5. Run 'postcss neo.css > neo.min.css' to create a minified version of the neo.css file for deployment
+6. The resulting files should also be added to the /neo_zip folder, along with the updated icons .css file, compressed, and then added to the website
+
+/////// TO-DO -- continue expanding on steps to update website
+
+In case icons need to be pulled:
+
+1. Run the 'icongen' script referenced above
+   **_ Important note _** save the icon file before running the following commands - this will ensure it is formatted correctly
+2. Run 'node -r esm icon-file-reader.js' in the API/icons-functions folder to retrieve the unicode for icons used directly in Neo's .scss files, and save them to a .json file. Note that you will have to run 'node -r esm styleDicExt.js' again to make sure the appropriate .scss file is generated.
+3. Run 'node -r esm neo-icon-combine.js' in the root directory to copy the new icons .css file into the appropriate .scss file in the /neo/scss-update folder. Now the icons will be added to the Neo.css file each time this is generated.
+4. As a second step, you will need to export the new icons from Figma in svg and png formats. These should be added to the appropriate sub-folder in the /resized-icons folder in the root directory, and then uploaded to the Firebase database along with the relevant .zip folders
+
+//////// TO-DO -- continue writing out steps as necessary
