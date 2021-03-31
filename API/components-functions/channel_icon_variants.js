8b1f46e27c082bf8bddd42b635b7dbfd55f59342
Razian <mrazian@avaya.com>
2021-03-31 11:21:54 -0400

    - Added code to Neo update e-mail notification template and Firebase function - Added code to pull and create Icon State icons from Figma - Added code to pull new Left Navigation styles from Figma - Created new active styles for Left Navigation sub items, created new ‘—no-icon’ class for sub items who’s parents have no icon - Corrected font-weight of Table Headers to semibold - Changed focus styles on Tooltips to focus-within - Added all necessary button styles to .nav-left class - Corrected unnecessary scale property that was flipping RTL text in Tooltips - Corrected bug in RTL styles of neo-form—inline that was causing an error in margins - Removed word-break property from notification counter - Added correct spacing for buttons in List Sections with Actions

---
+++ API/components-functions/channel_icon_variants.js
@@ -0,0 +1,172 @@
+const fs = require("fs").promises;
+const coreFigmaFunctions = require("../figma-functions/core-figma-functions.js");
+
+/*
+
+small size - DONE
+large size - DONE
+
+icon unicodes -- from icon-file-reader.js
+
+background color - DONE
+icon status colors - DONE
+
+these will need to go in one map
+
+*/
+
+async function channelIconVariants(value) {
+  channelIconJSONObject = {
+    channelIcon: {},
+  };
+
+  const channelIconStatus = value.Images.children.filter(
+    (child) => child.name === ".status"
+  )[0].children;
+
+  channelIconJSONObject.channelIcon["small-icon-font-size"] = {
+    value: `${
+      value.Images.children.filter(
+        (child) => child.name === ".icon-state-small"
+      )[0].size.x
+    }px`,
+  };
+
+  channelIconJSONObject.channelIcon["large-icon-font-size"] = {
+    value: `${
+      value.Images.children.filter(
+        (child) => child.name === ".icon-state-large"
+      )[0].size.x
+    }px`,
+  };
+
+  //   console.log(channelIconStatus);
+
+  // icon colours
+
+  for (status of channelIconStatus) {
+    // available color
+    if (status.name === "Type=available, Size=small") {
+      // small size
+      channelIconJSONObject.channelIcon["small-size"] = {
+        value: `${status.size.x}px`,
+      };
+      var statusColorTokenID =
+        status.children[0].children[1].children[0].styles.fill;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          statusColorTokenID
+        )
+        .then((value) => {
+          channelIconJSONObject.channelIcon["available-color"] = {
+            value: `{color.${value.nodes[
+              statusColorTokenID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+      var backgroundColorTokenID = status.children[0].children[0].styles.fill;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          backgroundColorTokenID
+        )
+        .then((value) => {
+          channelIconJSONObject.channelIcon["bg-color"] = {
+            value: `{color.${value.nodes[
+              backgroundColorTokenID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+    }
+    // busy/away colour
+    if (status.name === "Type=away, Size=small") {
+      var statusColorTokenID =
+        status.children[0].children[1].children[0].styles.fill;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          statusColorTokenID
+        )
+        .then((value) => {
+          channelIconJSONObject.channelIcon["busy-away-color"] = {
+            value: `{color.${value.nodes[
+              statusColorTokenID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+    }
+    if (status.name === "Type=do-not-disturb, Size=small") {
+      var statusColorTokenID =
+        status.children[0].children[1].children[0].styles.fill;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          statusColorTokenID
+        )
+        .then((value) => {
+          channelIconJSONObject.channelIcon["dnd-color"] = {
+            value: `{color.${value.nodes[
+              statusColorTokenID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+    }
+    if (status.name === "Type=offline, Size=small") {
+      var statusColorTokenID =
+        status.children[0].children[1].children[0].styles.fill;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          statusColorTokenID
+        )
+        .then((value) => {
+          channelIconJSONObject.channelIcon["offline-color"] = {
+            value: `{color.${value.nodes[
+              statusColorTokenID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+    }
+    if (status.name === "Type=lock, Size=small") {
+      var statusColorTokenID =
+        status.children[0].children[1].children[0].styles.fill;
+      await coreFigmaFunctions
+        .getFigmaTokenNameByID(
+          coreFigmaFunctions.figmaCredentials.figmaAPIKey,
+          coreFigmaFunctions.figmaCredentials.varaintComponentsFileID,
+          statusColorTokenID
+        )
+        .then((value) => {
+          channelIconJSONObject.channelIcon["generic-color"] = {
+            value: `{color.${value.nodes[
+              statusColorTokenID
+            ].document.name.toLowerCase()}.value}`,
+          };
+        });
+    }
+    if (status.name === "Type=available, Size=large") {
+      // large size
+      channelIconJSONObject.channelIcon["large-size"] = {
+        value: `${status.size.x}px`,
+      };
+    }
+  }
+
+  console.log(channelIconJSONObject);
+  await fs
+    .writeFile(
+      "../properties/components/channelIcons.json",
+      JSON.stringify(channelIconJSONObject)
+    )
+    .then(function () {
+      console.log("channelIcons.json created");
+    });
+}
+
+exports.channelIconVariants = channelIconVariants;
